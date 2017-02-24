8d34f85eed97ffc39176dba8f324d0e539130868
Thiago Rodrigues Meyer <Thiago Rodrigues Meyer>
2017-02-23 23:11:17 -0300

    create id for each cell the performance boost using getelementbyid instead of queryselector is huge!

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
-var width = 40,
-    height = 40,
+var height = 50,
+    width = 30,
     marking,
     action;
 
@@ -17,18 +17,22 @@ document.getElementById("gridContainer").addEventListener("click", function(e){
     
     if (e.target.classList.contains("cell")) {        
         e.target.classList.toggle("alive");        
-    }
-                    
-                    
+    }             
 });
 
 document.getElementById("start").addEventListener("click", function(){
     marking(width, height);
     action(width, height);
-    
 });
 
-
+document.getElementById("clear").addEventListener("click", function(){
+   
+    var cells = document.getElementById("gridContainer").children;
+    
+    for (let i = width*height; i >= 1; i--) {
+        cells[i].classList.remove("alive");
+    }
+});
 
 
 
@@ -36,40 +40,35 @@ document.getElementById("start").addEventListener("click", function(){
 
 
 grid = function createGrid(width, height) {
-    for (let i = height; i >= 1; i--) {
-        for (let j = width; j >= 1; j--) {            
+    for (let i = width; i >= 1; i--) {
+        for (let j = height; j >= 1; j--) {            
             
-            if (j == width) {
-                document.getElementById("gridContainer").insertAdjacentHTML("afterbegin",'<div class = "cell cell-' + i + '-' + j + '      -"></div><br>');
+            if (j == height) {
+                document.getElementById("gridContainer").insertAdjacentHTML("afterbegin",'<div id = "' + i + '-' + j + '" class = "cell"></div><br>');
             } else {
-                document.getElementById("gridContainer").insertAdjacentHTML("afterbegin",'<div class = "cell cell-' + i + '-' + j + '      -"></div>');
+                document.getElementById("gridContainer").insertAdjacentHTML("afterbegin",'<div id = "' + i + '-' + j + '" class = "cell"></div>');
             }
         }
     }
 }
 
-
-
-
-
-
-
 marking = function newCicle(width, height) {
     var adjacentLiveCells,    
         line,
         column,        
-        classSplit;
-    for (let i = height; i >= 1; i--) {
-        for (let j = width; j >= 1; j--) {            
+        idSplit;
+    for (let i = width; i >= 1; i--) {
+        for (let j = height; j >= 1; j--) {            
             adjacentLiveCells = 0;
             
             for (let adjX = -1; adjX <= 1; adjX++) {
                 for (let adjY = -1; adjY <= 1; adjY++) {
                     if (adjX != 0 || adjY != 0) {  
-                        classSplit = document.querySelector(".cell-" + i + "-" + j).classList.value.split("-");
-                        line = Number(classSplit[1]);
-                        column = Number(classSplit[2]);
-                        if ( (line + adjX > 0) && (column + adjY > 0) && (line + adjX <= width) && (column + adjY <= height) && document.querySelector(".cell-" + (line + adjX) + "-" + (column + adjY)).classList.contains("alive")) {
+                        idSplit = document.getElementById(i + "-" + j).id.split("-");
+                        console.log()
+                        line = Number(idSplit[0]);
+                        column = Number(idSplit[1]);
+                        if ( (line + adjX > 0) && (column + adjY > 0) && (line + adjX <= width) && (column + adjY <= height) && document.getElementById((line + adjX) + "-" + (column + adjY)).classList.contains("alive")) {
                             adjacentLiveCells++
                         } 
                     }
@@ -77,10 +76,10 @@ marking = function newCicle(width, height) {
             }
             //console.log("adjacent live cells" + adjacentLiveCells);
             if (adjacentLiveCells < 2 || adjacentLiveCells > 3) {
-                document.querySelector(".cell-" + i + "-" + j).classList.add("willDie");    
+                document.getElementById(i + "-" + j).classList.add("willDie");    
             } else if (adjacentLiveCells == 3) {
-                if (!document.querySelector(".cell-" + i + "-" + j).classList.contains("alive")); {
-                    document.querySelector(".cell-" + i + "-" + j).classList.add("becomeAlive");
+                if (!document.getElementById(i + "-" + j).classList.contains("alive")); {
+                    document.getElementById(i + "-" + j).classList.add("becomeAlive");
                 }
                 
             }
@@ -89,16 +88,16 @@ marking = function newCicle(width, height) {
 }
 
 action = function createGrid(width, height) {
-    for (let i = height; i >= 1; i--) {
-        for (let j = width; j >= 1; j--) {            
+    for (let i = width; i >= 1; i--) {
+        for (let j = height; j >= 1; j--) {            
             
-            if (document.querySelector(".cell-" + i + "-" + j).classList.contains("willDie")) {
-                document.querySelector(".cell-" + i + "-" + j).classList.remove("alive");
-                document.querySelector(".cell-" + i + "-" + j).classList.remove("willDie");
+            if (document.getElementById(i + "-" + j).classList.contains("willDie")) {
+                document.getElementById(i + "-" + j).classList.remove("alive");
+                document.getElementById(i + "-" + j).classList.remove("willDie");
                 
-            } else if (document.querySelector(".cell-" + i + "-" + j).classList.contains("becomeAlive")) {
-                document.querySelector(".cell-" + i + "-" + j).classList.add("alive");
-                document.querySelector(".cell-" + i + "-" + j).classList.remove("becomeAlive");
+            } else if (document.getElementById(i + "-" + j).classList.contains("becomeAlive")) {
+                document.getElementById(i + "-" + j).classList.add("alive");
+                document.getElementById(i + "-" + j).classList.remove("becomeAlive");
             }
         }
     }
